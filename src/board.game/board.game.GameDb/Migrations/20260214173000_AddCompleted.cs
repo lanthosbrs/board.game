ddfed35604c7842fd215c940ecc84e321f21fd15
lanthosbrs <brian@liquidfuture.net>
2026-02-14 12:32:14 -0500

    fixed the migration

---
+++ src/board.game/board.game.GameDb/Migrations/20260214173000_AddCompleted.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace board.game.GameDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ShelfNumber",
+                table: "Games",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShelfNumber",
+                table: "Games");
+        }
+    }
+}
